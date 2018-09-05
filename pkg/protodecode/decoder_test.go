be59a0402bb179eb433011bb6edf4b0bfe96c400
emicklei_kramphub <ernest@kramphub.com>
2018-09-05 22:25:16 +0200

    add repeated bool + add simple message

--- pkg/protodecode/decoder_test.go
+++ pkg/protodecode/decoder_test.go
@@ -18,10 +18,10 @@ func TestInt32(t *testing.T) {
 
 func TestRepeatedInt32(t *testing.T) {
 	m := &Test{
-		FieldsInt32: []int32{1, 2, 3, 4},
+		FieldInt32S: []int32{1, 2, 3, 4},
 	}
 	result := encodeDecode(m, t)
-	list := result["fields_int32"].([]interface{})
+	list := result["field_int32s"].([]interface{})
 	if got, want := len(list), 4; got != want {
 		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
 	}
@@ -42,10 +42,10 @@ func TestInt64(t *testing.T) {
 
 func TestRepeatedInt64(t *testing.T) {
 	m := &Test{
-		FieldsInt64: []int64{1, 2, 3, 4},
+		FieldInt64S: []int64{1, 2, 3, 4},
 	}
 	result := encodeDecode(m, t)
-	list := result["fields_int64"].([]interface{})
+	list := result["field_int64s"].([]interface{})
 	if got, want := len(list), 4; got != want {
 		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
 	}
@@ -66,10 +66,10 @@ func TestFloat(t *testing.T) {
 
 func TestRepeatedFloat(t *testing.T) {
 	m := &Test{
-		FieldsFloat: []float32{3.14, 0.234},
+		FieldFloats: []float32{3.14, 0.234},
 	}
 	result := encodeDecode(m, t)
-	list := result["fields_float"].([]interface{})
+	list := result["field_floats"].([]interface{})
 	if got, want := len(list), 2; got != want {
 		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
 	}
@@ -90,11 +90,10 @@ func TestString(t *testing.T) {
 
 func TestRepeatedString(t *testing.T) {
 	m := &Test{
-		FieldsString: []string{"hello", "world"},
+		FieldStrings: []string{"hello", "world"},
 	}
 	result := encodeDecode(m, t)
-	t.Logf("%#v", result)
-	list := result["fields_string"].([]interface{})
+	list := result["field_strings"].([]interface{})
 	if got, want := len(list), 2; got != want {
 		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
 	}
@@ -110,6 +109,34 @@ func TestBool(t *testing.T) {
 	}
 	result := encodeDecode(m, t)
 	if got, want := result["field_bool"], true; got != want {
+		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
+	}
+}
+
+func TestRepeatedBool(t *testing.T) {
+	m := &Test{
+		FieldBools: []bool{true, false},
+	}
+	result := encodeDecode(m, t)
+	list := result["field_bools"].([]interface{})
+	if got, want := len(list), 2; got != want {
+		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
+	}
+	if got, want := list[1].(bool), false; got != want {
+		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
+	}
+}
+
+func TestFoo(t *testing.T) {
+	m := &Test{
+		FieldFoo: &Foo{Foo: "foo"},
+	}
+	result := encodeDecode(m, t)
+	foo := result["field_foo"].(map[string]interface{})
+	if got, want := foo != nil, true; got != want {
+		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
+	}
+	if got, want := foo["foo"], "foo"; got != want {
 		t.Errorf("got %v (%T) %v (%T)", got, got, want, want)
 	}
 }
